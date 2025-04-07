102a4f044b25f1346c46f739757028695acd43ec
Saturn <sussycat2133@gmail.com>
2025-04-07 20:19:13 +0100

    Update dog.cmd

--- dog.cmd
+++ dog.cmd
@@ -2,14 +2,6 @@
 cd %USERPROFILE%\Documents\cmdDogCheck
 chcp 65001
 cls
-set "file=mus_sigh_of_dog.mp3"
-( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
-  echo Sound.URL = "%file%"
-  echo Sound.Controls.play
-  echo do while Sound.currentmedia.duration = 0
-  echo wscript.sleep 100
-  echo loop
-  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
 start /min sound.vbs
 @echo off
 cls
