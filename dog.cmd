950fc540635e46b6b2cca2031ea0354abfacb538
Saturn <sussycat2133@gmail.com>
2025-04-07 20:03:26 +0100

    Update dog.cmd

---
+++ dog.cmd
@@ -0,0 +1,113 @@
+
+@echo on
+chcp 65001
+cls
+set "file=mus_sigh_of_dog.mp3"
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
+@echo off
+cls
+goto dogframes
+
+
+:dogframes    
+                   
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
+type frame02.txt   
+timeout /t 1 /nobreak > NUL
+cls
+type frame01.txt                                                                                                    
+timeout /t 1 /nobreak > NUL
+cls
