98d4f22871faa76855c609d5357d1c538f8012a8
Saturn <sussycat2133@gmail.com>
2025-04-07 20:54:19 +0100

    Update dog.cmd

--- dog.cmd
+++ dog.cmd
@@ -1,11 +1,17 @@
-@echo on
+@echo off
+echo WARNING: This script attempts to close every single cmd window after its completion, if you have any work on neovim, a console based IDE, etc.. save your work and prepare for the dog
+echo.
+pause
 cd %USERPROFILE%\Documents\cmdDogCheck
+
+start /min "the sentient terminal" cmd /k "@echo off && echo easter egg hahahahahaha......... fuck off! i am a sentient terminal! just kidding.. im the sound system..&& powershell -c (New-Object Media.SoundPlayer "%USERPROFILE%\Documents\cmdDogCheck\mus_sigh_of_dog.wav").PlaySync(); %% exit"
+
 chcp 65001
 cls
-start /min sound.vbs
 @echo off
 cls
 goto dogframes
+
 
 :dogframes    
                    
@@ -102,4 +108,4 @@ cls
 type frame01.txt                                                                                                    
 timeout /t 1 /nobreak > NUL
 cls
-exit
+taskkill /f /im cmd.exe
